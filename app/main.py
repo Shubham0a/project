52d4096f49f344c81e5897c539cd697310daa97e
Shubham kumar <75657176+Shubham0a@users.noreply.github.com>
2024-11-13 16:34:26 +0000

    final commit

--- app/main.py
+++ app/main.py
@@ -11,5 +11,14 @@ db = client["user_data"]
 
 # client = MongoClient("mongodb+srv://shubhamiitdev:5KjsIw34uQJyfiUf@cluster1.vdq4y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
 
+@app.get("/")
+async def check_connection():
+    try:
+        client.server_info()  # This will raise an exception if the connection is not successful
+        return {"message": "Connected to MongoDB successfully!"}
+    except ConnectionError as e:
+        raise HTTPException(status_code=500, detail=f"Failed to connect to MongoDB: {str(e)}")
+
+
 app.include_router(auth.router, prefix="/auth")
 app.include_router(user.router, prefix="/users")
